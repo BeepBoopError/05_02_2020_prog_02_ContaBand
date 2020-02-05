b3d8b810120640a41b03baecf0860ed98f0633ec
BeepBoopError <emil0131@gmail.com>
2020-02-05 13:49:48 +0100

    Add project files.

---
+++ 05_02_2020_prog_02_ContaBand/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_02_2020_prog_02_ContaBand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
